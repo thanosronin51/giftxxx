34645a0d22b717cae7e2960351fa4181e0a7f259
thanosronin51 <88163214+thanosronin51@users.noreply.github.com>
2023-06-12 11:39:54 +0100

    Update models.py

--- giftweb/models.py
+++ giftweb/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     countdown_time = models.DurationField()
-    image = models.ImageField(upload_to='product_images')
+    image = models.ImageField(upload_to='product_images/')
     timestamp = models.DateTimeField(auto_now_add=True)
     discount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
@@ -21,7 +21,7 @@ class PremiumProduct(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     countdown_time = models.DurationField()
-    image = models.ImageField(upload_to='product_images')
+    image = models.ImageField(upload_to='product_images/')
     extra_feature = models.CharField(max_length=255)
     timestamp = models.DateTimeField(auto_now_add=True)
     discount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
@@ -70,7 +70,7 @@ class CartItem(models.Model):
 
 class Blog(models.Model):
     topic = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='blog_images')
+    image = models.ImageField(upload_to='blog_images/')
     body = RichTextField()
     date = models.DateField()
     read_more = models.CharField(max_length=255, default='GiftCard')
