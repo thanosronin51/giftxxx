20df3f010392eb9dca579dad8ba24eabdcd74432
thanosronin51 <88163214+thanosronin51@users.noreply.github.com>
2023-06-12 12:47:25 +0100

    Delete giftweb directory

--- giftweb/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Payment
-
-class PaymentForm(forms.ModelForm):
-    gift_card_type = forms.ChoiceField(choices=[('apple', 'Apple Card'), ('amazon', 'Amazon Card'), ('steam', 'Steam Card'), ('xbox', 'Xbox Card')])
-
-    class Meta:
-        model = Payment
-        fields = ['amount', 'proof_of_pay', 'gift_card_type']
--- giftweb/templatetags/custom_filters.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def total_seconds(duration):
-    return int(duration.total_seconds())
