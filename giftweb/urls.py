2c151897ea8ed9c327c75c1eb4d1f638630a6bc6
thanosronin51 <88163214+thanosronin51@users.noreply.github.com>
2023-06-26 06:30:14 +0100

    Update urls.py

--- giftweb/urls.py
+++ giftweb/urls.py
@@ -13,11 +13,12 @@ urlpatterns = [
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
 
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-    path('make_payment/<str:model_name>/<int:product_id>/', make_payment, name='make_payment'),
+    #path('make_payment/<str:model_name>/<int:product_id>/', make_payment, name='make_payment'),
     path('payment_success/', payment_success, name='payment_success'),
     path('premium_product/<int:pk>/', PremiumProductDetailView.as_view(), name='premium_product_detail'),
     
     # Add the following URL for payment history
     path('payment_history/', payment_history, name='payment_history'),
-
+    path('make_payment/product/<int:product_id>/', make_payment_for_product, name='make_payment_product'),
+    path('make_payment/premium_product/<int:premium_product_id>/', make_payment_for_premium_product, name='make_payment_premium_product'),
 ]
